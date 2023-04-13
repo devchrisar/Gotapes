af5692f68ca873b1d260f54a4a9f5cea6754f0fb
Devchrisar <christopher.ariasar@amigo.edu.co>
2023-04-13 11:29:11 -0500

    feat(controllers): Added new controller for handling register path

---
+++ controllers/register.Controller.go
@@ -0,0 +1,35 @@
+package controllers
+
+import (
+	"encoding/json"
+	"github.com/devchrisar/Gotapes/db"
+	"github.com/devchrisar/Gotapes/models"
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+func RegisterHandler(c echo.Context) error {
+	var t models.User
+	err := json.NewDecoder(c.Request().Body).Decode(&t)
+	if err != nil {
+		return c.String(400, "Error in the request"+err.Error())
+	}
+	if len(t.Email) == 0 {
+		return c.String(400, "Email is required")
+	}
+	if len(t.Password) < 6 {
+		return c.String(400, "Password is required")
+	}
+	_, found, _ := db.UserExists(t.Email)
+	if found {
+		return c.String(400, "User already exists")
+	}
+	_, status, err := db.InsertUser(t)
+	if err != nil {
+		return c.String(400, "An error occurred while trying to register the user"+err.Error())
+	}
+	if !status {
+		return c.String(400, "User could not be registered")
+	}
+	return c.String(http.StatusCreated, "")
+}
