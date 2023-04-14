5e5e4dd407e9c609c2e5e29119e546655241dc7b
Devchrisar <christopher.ariasar@amigo.edu.co>
2023-04-13 21:23:29 -0500

    fix(db): increased 'cost' for better security at the time of encryption

--- db/encryptPassword.go
+++ db/encryptPassword.go
@@ -3,7 +3,7 @@ package db
 import "golang.org/x/crypto/bcrypt"
 
 func EncryptPassword(password string) string {
-	cost := 8
+	cost := 10
 	bytes, _ := bcrypt.GenerateFromPassword([]byte(password), cost)
 	return string(bytes)
 }
