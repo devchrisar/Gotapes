1104ad06606cdd8fde84ccbd15420a2da5b57bbe
Devchrisar <christopher.ariasar@amigo.edu.co>
2023-04-16 16:08:14 -0500

    refactor(db): changes to improve readability by removing repeated 'if'

--- db/alterRegister.go
+++ db/alterRegister.go
@@ -13,32 +13,22 @@ func AlterRegister(user models.User, ID string) (bool, error) {
 	defer cancel()
 	db := MongoC.Database("Gotapes")
 	col := db.Collection("users")
-	register := make(map[string]interface{})
-	if len(user.Name) > 0 {
-		register["name"] = user.Name
+	update := bson.M{}
+	addField := func(fieldName string, fieldValue interface{}) {
+		if len(fieldValue.(string)) > 0 {
+			update[fieldName] = fieldValue
+		}
 	}
-	if len(user.LastName) > 0 {
-
-		register["lastName"] = user.LastName
-	}
-	register["birthDate"] = user.BirthDate
-	if len(user.Avatar) > 0 {
-		register["avatar"] = user.Avatar
-	}
-	if len(user.Banner) > 0 {
-		register["banner"] = user.Banner
-	}
-	if len(user.Bio) > 0 {
-		register["bio"] = user.Bio
-	}
-	if len(user.Location) > 0 {
-		register["location"] = user.Location
-	}
-	if len(user.WebSite) > 0 {
-		register["webSite"] = user.WebSite
-	}
+	addField("name", user.Name)
+	addField("lastName", user.LastName)
+	update["birthDate"] = user.BirthDate
+	addField("avatar", user.Avatar)
+	addField("banner", user.Banner)
+	addField("bio", user.Bio)
+	addField("location", user.Location)
+	addField("webSite", user.WebSite)
 	updateString := bson.M{
-		"$set": register,
+		"$set": update,
 	}
 	objID, _ := primitive.ObjectIDFromHex(ID)
 	filter := bson.M{"_id": bson.M{"$eq": objID}}
