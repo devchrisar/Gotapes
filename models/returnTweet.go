6efed728e223c6c9e3183ef603279692e612bff4
Devchrisar <christopher.ariasar@amigo.edu.co>
2023-04-20 22:40:54 -0500

    feat(db - models): Added model for receiving a tweet

---
+++ models/returnTweet.go
@@ -0,0 +1,13 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+type ReturnTweet struct {
+	ID      primitive.ObjectID `bson:"_id" json:"_id,omitempty"`
+	UserID  string             `bson:"userid" json:"userId,omitempty"`
+	Message string             `bson:"message" json:"message,omitempty"`
+	Date    time.Time          `bson:"date" json:"date,omitempty"`
+}
