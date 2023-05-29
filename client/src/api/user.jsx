6381dc52c47f329eea69c61382ddd3b1b5f28570
Devchrisar <christopher.ariasar@amigo.edu.co>
2023-05-29 11:14:02 -0500

    refactor(client): Improve API functions in user.jsx

--- client/src/api/user.jsx
+++ client/src/api/user.jsx
@@ -1,22 +1,100 @@
 import { API_HOST } from "../utils/constant";
 import { getTokenApi } from "./auth";
 
-export function getUserApi(id) {
+export async function getUserApi(id) {
   const url = `${API_HOST}/visit-profile?id=${id}`;
-  const params = {
-    headers: {
-      Authorization: `Bearer${getTokenApi()}`,
-    },
+  const token = getTokenApi();
+  const headers = {
+    Authorization: `Bearer${token}`,
   };
-  return fetch(url, params)
-    .then((response) => {
-      if (response.status >= 400) throw null;
-      return response.json();
-    })
-    .then((result) => {
-      return result;
-    })
-    .catch((err) => {
-      return err;
-    });
+  const params = {
+    headers: headers,
+  };
+
+  try {
+    const response = await fetch(url, params);
+
+    if (response.status >= 400) {
+      throw new Error("Invalid token");
+    }
+
+    const result = await response.json();
+    return result;
+  } catch (err) {
+    return err;
+  }
+}
+
+export async function uploadBannerApi(file) {
+  const url = `${API_HOST}/upload_Banner`;
+  const token = getTokenApi();
+
+  const formData = new FormData();
+  formData.append("banner", file);
+
+  const headers = {
+    Authorization: `Bearer ${token}`,
+  };
+
+  const params = {
+    method: "POST",
+    headers: headers,
+    body: formData,
+  };
+
+  try {
+    const response = await fetch(url, params);
+    const result = await response.json();
+    return result;
+  } catch (err) {
+    return err.message;
+  }
+}
+
+export async function uploadAvatarApi(file) {
+  const url = `${API_HOST}/upload_Avatar`;
+  const token = getTokenApi();
+
+  const formData = new FormData();
+  formData.append("avatar", file);
+
+  const headers = {
+    Authorization: `Bearer ${token}`,
+  };
+
+  const params = {
+    method: "POST",
+    headers: headers,
+    body: formData,
+  };
+
+  try {
+    const response = await fetch(url, params);
+    const result = await response.json();
+    return result;
+  } catch (err) {
+    return err.message;
+  }
+}
+
+export async function updateInfoApi(data) {
+  const url = `${API_HOST}/alter-profile`;
+  const token = getTokenApi();
+  const headers = {
+    Authorization: `Bearer ${token}`,
+  };
+
+  const params = {
+    method: "PUT",
+    headers: headers,
+    body: JSON.stringify(data),
+  };
+
+  try {
+    const response = await fetch(url, params);
+    const result = await response.json();
+    return result;
+  } catch (err) {
+    return err.message;
+  }
 }
