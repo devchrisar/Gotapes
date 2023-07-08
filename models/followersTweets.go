1d9e4962168178e554ac149ec8f05cb634721db0
Devchrisar <christopher.ariasar@amigo.edu.co>
2023-07-07 21:59:03 -0500

    fix: Fixed an error where the gifs were not displayed in the home

--- models/followersTweets.go
+++ models/followersTweets.go
@@ -10,5 +10,6 @@ type FollowersTweets struct {
 		Message string    `bson:"message" json:"message,omitempty"`
 		Date    time.Time `bson:"date" json:"date,omitempty"`
 		ID      string    `bson:"_id" json:"_id,omitempty"`
+		GIFurl  string    `bson:"gifurl" json:"gifurl,omitempty"`
 	}
 }
